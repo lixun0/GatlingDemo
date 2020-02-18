aba0388432a0f4f65ffa7b552f6f5bf56794bdc8
xunli <xunli@freewheel.tv>
2020-02-17 21:26:50 -0500

    1 min

--- src/test/scala/frontline/sample/BasicSimulation.scala
+++ src/test/scala/frontline/sample/BasicSimulation.scala
@@ -23,6 +23,6 @@ class BasicSimulation extends Simulation {
     )
 
   setUp(
-    scn.inject(constantUsersPerSec(50*1000) during (10 seconds))
+    scn.inject(constantUsersPerSec(50*1000) during (60 seconds))
   ).protocols(httpConf)
 }
