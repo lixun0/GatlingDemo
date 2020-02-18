240bd90b5f3faaa2cc42a2516112748b049f0583
xunli <xunli@freewheel.tv>
2020-02-17 23:45:39 -0500

    gzip only

--- src/test/scala/frontline/sample/BasicSimulation.scala
+++ src/test/scala/frontline/sample/BasicSimulation.scala
@@ -15,7 +15,7 @@ class BasicSimulation extends Simulation {
     .baseUrl("https://loadtest.v.fwmrm.net")
     .userAgentHeader("freewheel")
     .header("Keep-Alive", "6000000")
-    .header("Accept-Encoding", "gzip,deflate")
+    .header("Accept-Encoding", "gzip")
     .maxConnectionsPerHost(1)
 
   val scn = scenario("Load Test")
