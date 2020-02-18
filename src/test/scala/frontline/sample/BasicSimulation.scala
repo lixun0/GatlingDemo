b626b7455e3a52381dcd5f5be700af5a0ad2b968
xunli <xunli@freewheel.tv>
2020-02-17 22:25:21 -0500

    change throttle

--- src/test/scala/frontline/sample/BasicSimulation.scala
+++ src/test/scala/frontline/sample/BasicSimulation.scala
@@ -28,7 +28,7 @@ class BasicSimulation extends Simulation {
   setUp(
     scn.inject(constantUsersPerSec(qps) during (60 seconds))
   ).protocols(httpConf).throttle(
-    reachRps(100*1000*10) in (10 seconds),
+    reachRps(qps) in (30 seconds),
     holdFor(2 minutes)
   )
 }
